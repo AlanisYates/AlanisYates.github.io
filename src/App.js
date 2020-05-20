97271a1509768f82e6fa3527f7966152c4a34eb1
Alanis Yates <yatesalanis@gmail.com>
2020-05-20 15:39:50 -0400

    Adding in responsive NavBar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import './App.css';
-import Button from 'react-bootstrap/Button';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const App = () =>  {
+const App = () => {
   return (
     <div className="App">
-
+      <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
+        <Navbar.Brand href="#home">Alanis Yates</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav" >
+          <Nav className="ml-auto">
+            <Nav.Link href="#features">Projects</Nav.Link>
+            <Nav.Link href="#experience">Experience</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     </div>
   );
 }
