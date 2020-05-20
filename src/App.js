624799353be283c3cc2787e6f4cd89c89803d407
Alanis Yates <yatesalanis@gmail.com>
2020-05-20 15:23:44 -0400

    cleaning up react and adding in modules

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
+const App = () =>  {
   return (
     <div className="App">
-<h1>Hello world</h1>
+
     </div>
   );
 }
