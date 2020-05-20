d86d2fb5b5e3154048f04a32337a1abd1e65a8f8
Alanis Yates <yatesalanis@gmail.com>
2020-05-20 15:59:41 -0400

    setting up the routes with the react-router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import React from 'react';
+import { Home } from './Components/Home';
 import './App.css';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom';
+import { Projects } from './Components/Projects';
+import { Experiencs } from './Components/Experiencs';
 
 const App = () => {
   return (
     <div className="App">
-      <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
-        <Navbar.Brand href="#home">Alanis Yates</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav" >
-          <Nav className="ml-auto">
-            <Nav.Link href="#features">Projects</Nav.Link>
-            <Nav.Link href="#experience">Experience</Nav.Link>
-            <Nav.Link href="#contact">Contact</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+      <Router>
+        <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
+          <Navbar.Brand as={Link} to="/">Alanis Yates</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav" >
+            <Nav className="ml-auto">
+              <Nav.Link as={Link} to="/projects">Projects</Nav.Link>
+              <Nav.Link as={Link} to="/experience">Experience</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/projects">
+            <Projects />
+          </Route>
+          <Route exact path="/experience">
+            <Experiencs />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/Components/Projects.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Projects = () => {
+    return (
+        <div>
+            <h1>This is the Project Page</h1>
+        </div>
+    )
+}
