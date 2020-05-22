c9c7c2f4649897eb99f03594a9e7cd4ac2542a70
AlanisYates <yatesalanis@gmail.com>
2020-05-21 23:27:37 -0400

    fixing the modals

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -8,10 +8,21 @@ import "./Project.css";
 const width = "100%";
 
 export const Projects = () => {
-  const [show, setShow] = useState(false);
+  const [modalState, setModalState] = useState(
+    "modal-one" | "modal-two" | ("close" > "close")
+  );
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+  const handleShowModalOne = () => {
+    setModalState("modal-one");
+  };
+
+  const handleShowModalTwo = () => {
+    setModalState("modal-two");
+  };
+
+  const handleClose = () => {
+    setModalState("close");
+  };
 
   return (
     <div
@@ -64,13 +75,13 @@ export const Projects = () => {
             <Button
               variant="light"
               style={{ fontSize: "1.1rem", fontWeight: "700" }}
-              onClick={handleShow}
+              onClick={handleShowModalOne}
             >
               More Info
             </Button>
 
             <Modal
-              show={show}
+              show={modalState === "modal-one"}
               onHide={handleClose}
               size="md"
               aria-labelledby="contained-modal-title-vcenter"
@@ -89,117 +100,14 @@ export const Projects = () => {
                     }}
                   >
                     A web app built with React that allows the user to create
-                    spotify playlist on their account all on the same screen.{" "}
+                    spotify playlist. Users are authenticated with their spotify account. The user can then seach for any song on spotify, add that song to a playlist and save that playlist to their account.{" "}
                   </p>
-
                   <p
                     style={{
                       fontWeight: "500",
                     }}
                   >
-                    Utilized react components, state management, HTTP and JSON
-                    requests to create a dynamic web application. Created and
-                    implemented a responsive website design with SCSS.
-                  </p>
-
-                  <hr />
-                  <h4 style={{ fontWeight: "900", fontSize: "1.5rem" }}>
-                    Built With:
-                  </h4>
-                  <ul
-                    style={{
-                      fontWeight: "500",
-                      listStyle: "square",
-                    }}
-                  >
-                    <li>React.js</li>
-                    <li>Spotify Api</li>
-                    <li>Node.js</li>
-                    <li>SCSS/SASS</li>
-                  </ul>
-                </div>
-              </Modal.Body>
-              <Modal.Footer>
-                <Button
-                  href="https://github.com/pokalama/Spot-"
-                  target="_blank"
-                  onClick={handleClose}
-                  style={{
-                    background: "black",
-                    border: "none",
-                    padding: "0.5rem 1rem",
-                  }}
-                >
-                  View Code
-                </Button>
-              </Modal.Footer>
-            </Modal>
-          </Card.Body>
-        </Card>
-        <Card
-          bg="dark"
-          text="light"
-          style={{
-            width: width,
-          }}
-        >
-          <Card.Img variant="top" src={require("./Images/rest.jpg")} />
-          <Card.Body>
-            <Card.Title>
-              <h2 style={{ fontWeight: "700" }}>Spot-On</h2>
-            </Card.Title>
-            <Button
-              href="https://spot-on.netlify.app/"
-              target="_blank"
-              onClick={handleClose}
-              variant="light"
-              style={{
-                marginRight: "1rem",
-                fontSize: "1.1rem",
-                fontWeight: "700",
-              }}
-            >
-              Live Demo
-            </Button>
-            <Button
-              variant="light"
-              style={{ fontSize: "1.1rem", fontWeight: "700" }}
-              onClick={handleShow}
-            >
-              More Info
-            </Button>
-
-            <Modal
-              show={show}
-              onHide={handleClose}
-              size="md"
-              aria-labelledby="contained-modal-title-vcenter"
-              centered
-            >
-              <Modal.Header closeButton>
-                <Modal.Title id="example-custom-modal-styling-title">
-                  <h1 style={{ fontWeight: "900" }}>Spot-On</h1>
-                </Modal.Title>
-              </Modal.Header>
-              <Modal.Body>
-                <div>
-                  <p
-                    style={{
-                      fontWeight: "500",
-                    }}
-                  >
-                    A web app built with React that allows the user to create
-                    spotify playlist on their account all on the same screen.{" "}
-                  </p>
-
-                  <p
-                    style={{
-                      fontWeight: "500",
-                    }}
-                  >
-                    Utilized react components, state management, HTTP and JSON
-                    requests to create a dynamic web application. Created and
-                    implemented a responsive website design with SCSS.
+                    {" "}
                   </p>
 
                   <hr />
@@ -246,10 +154,10 @@ export const Projects = () => {
           <Card.Img variant="top" src={require("./Images/Product Page.png")} />
           <Card.Body>
             <Card.Title>
-              <h2 style={{ fontWeight: "700" }}>Spot-On</h2>
+              <h2 style={{ fontWeight: "700" }}>File Deliver</h2>
             </Card.Title>
             <Button
-              href="https://spot-on.netlify.app/"
+              href="https://laughing-murdock-3b6a46.netlify.app/"
               target="_blank"
               onClick={handleClose}
               variant="light"
@@ -264,13 +172,13 @@ export const Projects = () => {
             <Button
               variant="light"
               style={{ fontSize: "1.1rem", fontWeight: "700" }}
-              onClick={handleShow}
+              onClick={handleShowModalTwo}
             >
               More Info
             </Button>
 
             <Modal
-              show={show}
+              show={modalState === "modal-two"}
               onHide={handleClose}
               size="md"
               aria-labelledby="contained-modal-title-vcenter"
@@ -288,19 +196,14 @@ export const Projects = () => {
                       fontWeight: "500",
                     }}
                   >
-                    A web app built with React that allows the user to create
-                    spotify playlist on their account all on the same screen.{" "}
+                    This is a static landing page for a file delivery app.{" "}
                   </p>
 
                   <p
                     style={{
                       fontWeight: "500",
                     }}
-                  >
-                    Utilized react components, state management, HTTP and JSON
-                    requests to create a dynamic web application. Created and
-                    implemented a responsive website design with SCSS.
-                  </p>
+                  ></p>
 
                   <hr />
                   <h4 style={{ fontWeight: "900", fontSize: "1.5rem" }}>
@@ -312,10 +215,9 @@ export const Projects = () => {
                       listStyle: "square",
                     }}
                   >
-                    <li>React.js</li>
-                    <li>Spotify Api</li>
-                    <li>Node.js</li>
-                    <li>SCSS/SASS</li>
+                    <li>HTML</li>
+                    <li>Javascript</li>
+                    <li>SCSS/CSS</li>
                   </ul>
                 </div>
               </Modal.Body>
